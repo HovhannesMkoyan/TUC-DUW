72c219ed4c41d6e0e76e3e3ed2a3a26996deaf10
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-06-11 23:27:30 +0400

    dropzone design

--- web-app/src/components/main/Dropzone.tsx
+++ web-app/src/components/main/Dropzone.tsx
@@ -2,7 +2,7 @@ import { useDropzone } from "react-dropzone";
 
 export default function Dropzone(props: any) {
   const { getRootProps, getInputProps, acceptedFiles } = useDropzone({});
-  
+
   const files = acceptedFiles.map((file: any) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
@@ -14,10 +14,22 @@ export default function Dropzone(props: any) {
       <div {...getRootProps({ className: "dropzone" })}>
         <input {...getInputProps()} />
         <div>
-          <div style={{position: "relative"}}>
-            <img src="/images/dropzone-image-shadowed.png" alt="" className="dropzone-img dropzone-img-effect dropzone-img-effect-left" />
-            <img src="/images/dropzone-image.png" alt="" className="dropzone-img" />
-            {/* <img src="/images/dropzone-image-shadowed.png" alt="" className="dropzone-img dropzone-img-effect dropzone-img-effect-right" /> */}
+          <div style={{ position: "relative" }}>
+            <img
+              src="/images/dropzone-image-shadowed.png"
+              alt=""
+              className="dropzone-img dropzone-img-effect dropzone-img-effect-left"
+            />
+            <img
+              src="/images/dropzone-image.png"
+              alt=""
+              className="dropzone-img"
+            />
+            <img
+              src="/images/dropzone-image-shadowed.png"
+              alt=""
+              className="dropzone-img dropzone-img-effect dropzone-img-effect-right"
+            />
           </div>
           <p>Drop your files here, or browse</p>
         </div>
