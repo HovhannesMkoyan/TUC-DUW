05d4fb4631201d1b9f15dd5bc889e3a37668f883
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-06-14 16:08:05 +0400

    dropzone component adjustment

--- web-app/src/components/main/Dropzone.tsx
+++ web-app/src/components/main/Dropzone.tsx
@@ -6,11 +6,6 @@ import getFileExtensionIcon from "../../helpers/get-file-extension-icon";
 export default function Dropzone() {
   const [selectedFiles, setSelectedFiles] = useState<any[]>([]);
 
-  let totalSize = 0;
-  for (let index = 0; index < selectedFiles.length; index++) {
-    totalSize += selectedFiles[index].size;
-  }
-
   const onDrop = useCallback(
     (acceptedFiles: any) => {
       setSelectedFiles([...selectedFiles, ...acceptedFiles]);
@@ -20,15 +15,8 @@ export default function Dropzone() {
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
+    multiple: false,
   });
-
-  const removeFile = (file: any) => () => {
-    const newFiles = [...selectedFiles];
-    newFiles.splice(newFiles.indexOf(file), 1);
-    setSelectedFiles(newFiles);
-  };
-
-  const removeAll = () => setSelectedFiles([]);
 
   return (
     <div className="dropzone-container">
@@ -71,35 +59,26 @@ export default function Dropzone() {
               src="/images/remove.png"
               alt="remove selected file icon"
               style={{ cursor: "pointer" }}
-              onClick={removeFile(file)}
+              onClick={() => setSelectedFiles([])}
             />
           </div>
         ))}
       </div>
-      {selectedFiles.length > 0 && (
-        <>
-          <div className="df selected-files-stats-container">
-            <p>
-              {selectedFiles.length}{" "}
-              {`${selectedFiles.length > 1 ? "files" : "file"}`}
-            </p>
-            <p>{(totalSize / 1000000).toPrecision(2)} MB</p>
-          </div>
-          {+(totalSize / 1000000).toPrecision(2) > 10 && (
-            <p className="selected-files-size-exceed">
-              Total size exceeds 10 MB. Please remove some files.
-            </p>
-          )}
-        </>
-      )}
 
       {selectedFiles.length > 0 && (
-        <div className="selected-files-actions-container">
-          <button onClick={removeAll}>Remove All</button>
-          {+(totalSize / 1000000).toPrecision(2) <= 10 && (
-            <button>Upload & Get URL</button>
+        <>
+          {+(selectedFiles[0].size / 1000000).toPrecision(2) > 10 && (
+            <p className="selected-files-size-exceed">
+              File size exceeds 10 MB. Please choose another file.
+            </p>
           )}
-        </div>
+
+          {+(selectedFiles[0].size / 1000000).toPrecision(2) <= 10 && (
+            <button style={{ marginTop: "20px", padding: "10px 15px" }}>
+              Upload & Get URL
+            </button>
+          )}
+        </>
       )}
     </div>
   );
--- web-app/src/helpers/get-adjusted-filename.ts
+++ web-app/src/helpers/get-adjusted-filename.ts
@@ -1,7 +1,7 @@
 export default (filename: string): string => {
   let fileExtension = filename.slice(filename.lastIndexOf(".") + 1);
 
-  return filename.length > 30
-    ? filename.substring(0, 30 - 1) + `...${fileExtension}`
+  return filename.length > 35
+    ? filename.substring(0, 35 - 1) + `...${fileExtension}`
     : filename;
 };
