6ba7637c0c665604039b292d66fc1e4f16f0c111
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-06-22 17:26:00 +0400

    file-container design

--- web-app/src/components/SingleFile/SingleFile.tsx
+++ web-app/src/components/SingleFile/SingleFile.tsx
@@ -3,8 +3,12 @@ import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFilePdf,
-  faCloudArrowDown
+  faCloudArrowDown,
+  faCalendarDays,
+  faLock,
+  faLockOpen,
 } from "@fortawesome/free-solid-svg-icons";
+import { Menu } from "@mantine/core";
 
 import { get, downloadFile } from "../../services/file.service";
 import { fetchFileKey, downloadFileKey } from "../../utils/queryKeys";
@@ -12,6 +16,7 @@ import formatDate from "../../helpers/format-date";
 import FileIcon from "../Helpers/FileIcon/FileIcon";
 import InPageLoader from "../Helpers/in-page-loader/InPageLoader";
 import Modal from "../Helpers/Modal/Modal";
+import Tooltip from "../Helpers/Tooltip/Tooltip";
 import "./SingleFile.css";
 
 export default function SingleFile(props: any) {
@@ -25,7 +30,6 @@ export default function SingleFile(props: any) {
   } = useQuery([fetchFileKey, uuid], () => get(uuid!), {
     staleTime: 1000 * 60 * 30,
   });
-  // formatDate(new Date(file.createdAt))
 
   return (
     <section className="main-container">
@@ -40,6 +44,27 @@ export default function SingleFile(props: any) {
       )}
       {isSuccess && (
         <div className="file-container">
+          <div className="file-menu">
+            <Menu
+              trigger="click"
+              delay={500}
+              size={250}
+              placement="center"
+              gutter={-5}
+              withArrow
+            >
+              <Menu.Item
+                disabled
+                icon={<FontAwesomeIcon icon={faCalendarDays} />}
+              >
+                {formatDate(new Date(file.createdAt))}
+              </Menu.Item>
+              <Menu.Item icon={<FontAwesomeIcon icon={faLock} />}>
+                Request for Blocking
+              </Menu.Item>
+            </Menu>
+          </div>
+
           <FileIcon filename={file.name} />
           <p className="file-title">{file.name}</p>
           {file.description && <p>{file.description}</p>}
@@ -48,14 +73,14 @@ export default function SingleFile(props: any) {
               <span className="bold">Filesize:</span>
               <span>{Math.round(file.size / 1000)} KB</span>
             </div>
-            <div className="df df-column upload-date">
-              <span className="bold">Upload date:</span>
-              <span>{formatDate(new Date(file.createdAt))}</span>
-            </div>
-            {/* <button className="download-file-btn">Download</button> */}
-            <FontAwesomeIcon icon={faCloudArrowDown} className="download-file-btn" />
+            <Tooltip text="Download file">
+            <FontAwesomeIcon
+              icon={faCloudArrowDown}
+              className="download-file-btn"
+            />
+          </Tooltip>
+            
           </div>
-          <p className="request-msg"></p>
         </div>
       )}
     </section>
