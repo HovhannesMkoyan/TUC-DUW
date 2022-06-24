0af757132b5d927ec1a7a4cb1ff8e5da0b8aa291
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-06-24 10:57:54 +0400

    refactoring

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
 import { app, sequelize } from "./server";
 import { wtcLogin } from "./src/helpers/wtc-login";
 
+const port = process.env.PORT;
 sequelize
   .sync()
-  .then(() => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Server is running on port ${process.env.PORT}`)
-    );
-  })
+  .then(() =>
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  )
   .then(() => wtcLogin(() => console.log("Logged in into WTC")))
   .catch((e: any) => console.log(e));
